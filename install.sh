6a0efa20a36cb336a55d8f0c5b051e178744a69e
chinganc <chinganc0@gmail.com>
2022-11-02 12:56:45 -0700

    add mujoco installation in install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
+################ Uncommment this part to install mujoco210
+sudo apt-get install -y libglew-dev patchelf libosmesa6-dev
+wget https://github.com/deepmind/mujoco/releases/download/2.1.0/mujoco210-linux-x86_64.tar.gz
+tar -xf mujoco210-linux-x86_64.tar.gz
+rm mujoco210-linux-x86_64.tar.gz
+mkdir ~/.mujoco
+mv mujoco210 ~/.mujoco
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/.mujoco/mujoco210/bin:/usr/lib/nvidia
+################
+
 pip install -r requirement.txt
 
 git clone https://github.com/chinganc/mjrl.git
